f8528fc24581453f2c0d0324bb1df95ce30d084b
gracekim527 <lemonherb0323@naver.com>
2024-11-02 01:11:37 +0900

    :sparkles: Feat: 팀원 초대 페이지 구현 (#6)

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Create from "@pages/create/Create";
+import Invite from "@pages/invite/Invite";
 
 const Router: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const Router: React.FC = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/invite" element={<Invite />} />
       </Routes>
     </BrowserRouter>
   );
